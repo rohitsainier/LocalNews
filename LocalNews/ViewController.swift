e8c175b1d538ab91440d8e27ce39452444bc686c
Rohit Saini <sketchlearn7@gmail.com>
2019-09-23 23:37:58 +0530

    Initial Commit

---
+++ LocalNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalNews
+//
+//  Created by Rohit Saini on 23/09/19.
+//  Copyright © 2019 Rohit Saini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
