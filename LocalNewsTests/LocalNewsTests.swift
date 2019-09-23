571510d8afb97aa9de946b1a44077ebab7d44d89
Rohit Saini <sketchlearn7@gmail.com>
2019-09-24 00:19:34 +0530

    Scheme commit

--- LocalNewsTests/LocalNewsTests.swift
+++ LocalNewsTests/LocalNewsTests.swift
@@ -22,13 +22,10 @@ class LocalNewsTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        var i = 0
+        xcas
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
